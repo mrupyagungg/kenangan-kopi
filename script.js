7ecce02b3beb1ede8a1ae400e3a4453791baecb7
mrupyagungg <mrrupyagung@gmail.com>
2024-06-08 22:12:47 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu')
+    .onclick = () => {
+        navbarNav.classList.toggle('active');
+    };
+
+// hamburger menu  
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) { navbarNav.classList.remove('active') }
+});
