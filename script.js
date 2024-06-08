baa497f184a4c5560539371152c27e2b371d5f51
mrupyagungg <mrrupyagung@gmail.com>
2024-06-08 23:28:34 +0700

    updating

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const navbarNav = document.querySelector('.navbar-nav');
 
-document.querySelector('#hamburger-menu')
-    .onclick = () => {
-        navbarNav.classList.toggle('active');
-    };
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
 
-// hamburger menu  
+// active search
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    e.preventDefault(); // Prevent default action to avoid unintended behaviors
+    searchForm.classList.toggle('active');
+};
+
+// hamburger menu
 const hamburger = document.querySelector('#hamburger-menu');
 
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) { navbarNav.classList.remove('active') }
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if (
+        !searchForm.contains(e.target) &&
+        !document.querySelector('#search-button').contains(e.target)
+    ) {
+        searchForm.classList.remove('active');
+    }
 });
