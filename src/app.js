5cf92620e8e6985a970b80813c135d3af1393380
mrupyagungg <mrrupyagung@gmail.com>
2024-06-13 23:27:09 +0700

    updating

--- src/app.js
+++ src/app.js
@@ -6,9 +6,19 @@ document.addEventListener('alpine:init', () => {
             { id: 3, name: 'Espresso', img: '3.jpg', price: '20000' },
             { id: 4, name: 'Caffé Latte', img: '4.jpg', price: '33000' },
             { id: 5, name: 'Mocca', img: '5.jpg', price: '34000' },
+            { id: 5, name: 'Machiato', img: '6.jpg', price: '24000' },
         ],
     }));
 });
 
 // Initialize Feather icons
 feather.replace();
+
+// rupiah
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(number);
+};
